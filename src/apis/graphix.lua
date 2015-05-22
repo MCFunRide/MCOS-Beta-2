649e3efa69e9bcbfcd5d44f7e071e239edc09769
MCFunRide <funridedoesminecraft@gmail.com>
2015-05-22 12:44:41 +0100

    Create graphix.lua

---
+++ src/apis/graphix.lua
@@ -0,0 +1,20 @@
+-- Graphix API v1: Coded by datcoder1208 
+
+
+function center(yPos,sText)
+  local w, h = term.getSize()
+  local x = term.getCursorPos()
+  x = math.max(math.floor((w / 2) - (#sText / 2)), 0)
+  term.setCursorPos(x,yPos)
+  print(sText)
+end
+ 
+function totalClear()
+  term.setCursorPos(1,1)
+  term.clear()
+end
+ 
+function bgColor(color)
+  term.setBackgroundColor(color)
+  totalClear()
+end
