3ccba85483a0d41dc478ffe00301c819930e386e
MCFunRide <funridedoesminecraft@gmail.com>
2015-05-22 12:42:54 +0100

    Create login.lua

---
+++ src/MCOS/login.lua
@@ -0,0 +1,15 @@
+os.loadAPI("/MCOS/sha256")
+os.loadAPI("/apis/graphix")
+graphix.bgColor(colors.blue)
+graphix.center(1,"MCOS Login")
+graphix.centerWrite(3,"Password: ")
+pass = read('*')
+main = sha256.sha256(pass)
+f = fs.open("/MCOS/.pass","r")
+m = f.readAll()
+ 
+if main == m then
+  shell.run("/MCOS/desktop")
+else
+  shell.run("/MCOS/login")
+end
