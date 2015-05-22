2645a9e9b9288281d7fb75c745b124ce2b164a46
MCFunRide <funridedoesminecraft@gmail.com>
2015-05-22 19:01:33 +0100

    Fixing Error
    Just realised that if read is empty it prints the help info for the command.

--- src/MCOS/editor.lua
+++ src/MCOS/editor.lua
@@ -4,4 +4,8 @@ graphix.bgColor(colors.green)
 graphix.center(1,"Welcome to the MCOS Editor!")
 graphix.centerWrite(5,"File Dir: /")
 file = read()
+if not file == nil then
 shell.run("edit "..file)
+else
+shell.run("/MCOS/editor")
+end
