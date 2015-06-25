2d7dbe2d4bcadd68bdf284d357f3070d134cfc4b
MCFunRide <funridedoesminecraft@gmail.com>
2015-06-25 21:13:32 +0100

    MCOS BETA 3 DESKTOP UPDATE
    This little update will probably dump a few of the pretty basic functions of the OS  until I get enough time to actually make a taskbar list for it all and it won't be using Graphix.

--- src/MCOS/desktop.lua
+++ src/MCOS/desktop.lua
@@ -11,49 +11,69 @@ OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTIO
 WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 --]]
 
-os.loadAPI("/apis/graphix")
-graphix.totalClear()
-graphix.bgColor(colors.lightBlue)
-term.setCursorPos(1,1)
-term.setBackgroundColor(colors.blue)
-term.clearLine()
-term.setTextColor(colors.red)
-graphix.centerWrite(1,"MCOS Beta "..version)
-term.setBackgroundColor(colors.lightBlue)
-print()
-term.setCursorPos(1,3)
-term.setTextColor(colors.yellow)
-term.setBackgroundColor(colors.green)
-graphix.center(3,"App Editor")
-graphix.center(5,"File Manager")
-graphix.center(7,"Games")
-graphix.center(9,"Power (i)")
-graphix.center(11,"Shell")
-graphix.center(13,"Information")
-graphix.center(15,"Settings")
-graphix.center(17,"Update")
+-- StartGUI v2
+-- Original Idea by Gonow32
+-- Coded by datcoder1208
  
+-- Functions
+ 
+local w, h = term.getSize()
+ 
+function drawMenu()
+  term.setBackgroundColor(colors.cyan)
+  term.setCursorPos(1,h-1)
+  term.write("  Exit  ")
+  term.setCursorPos(1,h-3)
+  term.write("Explorer")
+  term.setCursorPos(1,h-2)
+  term.write("Settings")
+  term.setCursorPos(1,h-4)
+  term.write("NoteMake")
+  term.setCursorPos(1,h-5)
+  term.write("  Info  ")
+  term.setCursorPos(1,h-6)
+  term.write(" OSUser ")
+end
+ 
+function drawDesktop()
+  term.setBackgroundColor(colors.white)
+  term.clear()
+  term.setCursorPos(1,19)
+  term.setBackgroundColor(colors.blue)
+  term.clearLine()
+  term.setCursorPos(1,19)
+  term.setBackgroundColor(colors.green)
+  term.setTextColor(colors.black)
+  write("Start")
+end
+ 
+drawDesktop()
+menu = 0
 while true do
- 
   local evt, button, x, y = os.pullEvent("mouse_click")
- 
-  if y == 3 then
-    shell.run("/MCOS/editor")
-  elseif y == 5 then
-    shell.run("/MCOS/3rdParty/Gonow32/FileManager")
-  elseif y == 7 then
-    shell.run("/MCOS/GameList")
-  elseif y == 9 then
-    shell.run("/MCOS/power")
-  elseif y == 11 then
-    shell.run("/MCOS/shell")
-  elseif y == 13 then
-    shell.run("/MCOS/about")
-  elseif y == 15 then
+  if x >= 1 and x <= 5 and menu == 0 then
+    drawDesktop()
+    drawMenu()
+    menu = 1
+  elseif x >= 1 and x <= 5 and y == 19 and menu == 1 then
+    drawDesktop()
+    menu = 0
+  elseif x >= 1 and x <= 8 and y == h - 1 and menu == 1 then
+    os.shutdown()
+    menu = 0
+  elseif x <= 1 and x <= 8 and y == h - 2 and menu == 1 then
     shell.run("/MCOS/settings")
-  elseif y == 17 then
-    shell.run("/MCOS/update")
+    menu = 0
+  elseif x <= 1 and x <= 8 and y == h - 3 and menu == 1 then
+    shell.run("/MCOS/3rdParty/Gonow32/FileManager")
+    menu = 0
+  elseif x <= 1 and x <= 8 and y == h - 4 and menu == 1 then
+    shell.run("/MCOS/editor")
+    menu = 0
+  elseif x <= 1 and x <= 8 and y == h - 5 and menu == 1 then
+    shell.run("/MCOS/about")
   else
-    shell.run("/MCOS/desktop")
+    drawDesktop()
+    menu = 0
   end
 end
